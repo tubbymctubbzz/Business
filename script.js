2af608b2b542e842d4fd089e4cc226e6ff7b7577
Tubbzz <tubbzz18@gmail.com>
2024-08-18 11:07:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,35 @@ function scrollFunction() {
     document.getElementById("logo").style.fontSize = "25px";
   }
 }
+
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
+
+// menuIcon.onclick = () => {
+//   menuIcon.classList.toggle('bx-x');
+//   navbar.classList.toggle('active');
+// }
+
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('navbar a');
+
+// window.onscroll = () => {
+//   sections.forEach(sec => {
+//     let top = window.scrollY;
+//     let offset = sec.offsetTop - 100;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute('id');
+
+//     if(top >= offset && top < offset + height) {
+      
+//       navLinks.forEach(links => {
+//         links.classList.remove('active');
+//         document.querySelector(`navbar a[href=${id}]`).classList.add('active');
+//       });
+//       sec.classList.add('show-animate');
+//     }
+//     else {
+//       sec.classList.remove('show-animate');
+//     }
+//   });
+// }
