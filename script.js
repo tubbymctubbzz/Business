733c87dcdf2c6ace941626af945b37bc7b45b927
Tubbzz <tubbzz18@gmail.com>
2024-08-23 00:29:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// const loginOpnBtn = document.querySelector("#button-openform");
+// const loginBox = document.querySelector(".container-login");
+// const signupOpnBtn = document.querySelector(".button-login-signup");
+// const signupBox = document.querySelector(".container-signup");
+// const closebtn = document.querySelector(".closeButton");
+// const closebtnSignUp =document.querySelector(".closeButtonSignup");
+// const forgetPWLin =document.querySelector(".forgetPWLink");
+// const forgetP =document.querySelector(".forgetPW");
+// const closeButtonForgetP =document.querySelector(".closeButtonForgetPW");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -5,10 +14,11 @@ function scrollFunction() {
     document.getElementById("navbar").style.fontSize = "18px";
     document.getElementById("logo").style.fontSize = "18px";
   } else {
-    document.getElementById("navbar").style.fontSize = "25px";
-    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("navbar").style.fontSize = "23px";
+    document.getElementById("logo").style.fontSize = "23px";
   }
 }
+
 
 // let menuIcon = document.querySelector('#menu-icon');
 // let navbar = document.querySelector('.navbar');
@@ -42,103 +52,78 @@ function scrollFunction() {
 //   });
 // }
 
-const loginOpnBtn = document.querySelector("#button-openform");
-const loginBox = document.querySelector(".container-login");
-const signupOpnBtn = document.querySelector(".button-login-signup");
-const signupBox = document.querySelector(".container-signup");
-const closebtn = document.querySelector(".closeButton");
-const closebtnSignUp =document.querySelector(".closeButtonSignup");
-const forgetPWLin =document.querySelector(".forgetPWLink");
-const forgetP =document.querySelector(".forgetPW");
-const closeButtonForgetP =document.querySelector(".closeButtonForgetPW")
 
 
-loginOpnBtn.addEventListener("click", function () {
-    if (loginBox.style.display === "none" || loginBox.style.display === "") {
-        loginBox.style.display = "block";
-    } else {
-        loginBox.style.display = "none";
-    }
-});
+// loginOpnBtn.addEventListener("click", function () {
+//     if (loginBox.style.display === "none" || loginBox.style.display === "") {
+//         loginBox.style.display = "block";
+//     } else {
+//         loginBox.style.display = "none";
+//     }
+// });
 
-signupOpnBtn.addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent form submission
-    if (signupBox.style.display === "none" || signupBox.style.display === "") {
-        signupBox.style.display = "block";
-    } else {
-        signupBox.style.display = "none";
-    }
-    loginBox.style.display = "none";
-});
+// signupOpnBtn.addEventListener("click", function (event) {
+//     event.preventDefault(); // Prevent form submission
+//     if (signupBox.style.display === "none" || signupBox.style.display === "") {
+//         signupBox.style.display = "block";
+//     } else {
+//         signupBox.style.display = "none";
+//     }
+//     loginBox.style.display = "none";
+// });
 
 
-forgetPWLin.addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent form submission
-    if (forgetP.style.display === "none" || forgetP.style.display === "") {
-        forgetP.style.display = "block";
-    } else {
-        forgetP.style.display = "none";
-    }
-    loginBox.style.display = "none";
-    signupBox.style.display = "none"
-});
+// forgetPWLin.addEventListener("click", function (event) {
+//     event.preventDefault(); // Prevent form submission
+//     if (forgetP.style.display === "none" || forgetP.style.display === "") {
+//         forgetP.style.display = "block";
+//     } else {
+//         forgetP.style.display = "none";
+//     }
+//     loginBox.style.display = "none";
+//     signupBox.style.display = "none"
+// });
 
-closebtn.addEventListener("click", function () {
-    if(loginBox.style.display === "block")
-    {
-        loginBox.style.display = "none";
+// closebtn.addEventListener("click", function () {
+//     if(loginBox.style.display === "block")
+//     {
+//         loginBox.style.display = "none";
        
-    }
-})
+//     }
+// })
 
-closebtnSignUp.addEventListener("click", function(){
-    if(signupBox.style.display === "block")
-    {
-        signupBox.style.display = "none";
-    }
-})
+// closebtnSignUp.addEventListener("click", function(){
+//     if(signupBox.style.display === "block")
+//     {
+//         signupBox.style.display = "none";
+//     }
+// })
 
-closeButtonForgetP.addEventListener("click", function(){
-    if(forgetP.style.display === "block")
-    {
-        forgetP.style.display = "none";
-    }
-});
+// closeButtonForgetP.addEventListener("click", function(){
+//     if(forgetP.style.display === "block")
+//     {
+//         forgetP.style.display = "none";
+//     }
+// });
 
-// Smooth scrolling
+// // Smooth scrolling
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
 
-// Countdown timer
 
-<p id="demo"></p>
-
-var countDownDate = new Date("Jan 5, 2026 15:37:25").getTime();
-
-var x = setInterval(function() {
-
-  var now = new Date().getTime();
-
-  var distance = countDownDate - now;
-
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
+// Dialog box
+function openMe() {
+  document.querySelector('dialog').showModal()
+}
+function closeMe() {
+  document.querySelector('dialog').close()
+  window.location.href = 'https://tubbymctubbzz.github.io/Business/index.html'
+}
